889eabccba6469301ebf50c434717b52cad372ca
Jesus Magana <maganazero5@gmail.com>
2014-07-22 20:36:50 -0700

    Take in number and give to function

--- primeFactorization.c
+++ primeFactorization.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void returnFactorization();
+
 int main(int argc, char* argv[]){
-	printf("Hello World!");
+	int inputNumber = 0;
+	printf("Please input an integer value: ");
+   	scanf("%d", &inputNumber);
+   	returnFactorization(inputNumber);
 	return 0;
+}
+
+void returnFactorization(int inputNumber){
+	printf("Factors of %d: \n", inputNumber);
 }
