c74e14671219afa6cd0f7a3e719b51626662f0df
Jesus Magana <maganazero5@gmail.com>
2014-07-22 20:50:11 -0700

    Finished algorithm

--- primeFactorization.c
+++ primeFactorization.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+//function stub 
 void returnFactorization();
 
+//Just take in number and send to function
 int main(int argc, char* argv[]){
 	int inputNumber = 0;
 	printf("Please input an integer value: ");
    	scanf("%d", &inputNumber);
-   	returnFactorization(inputNumber);
+   	if(inputNumber == 1){
+   		printf("1");
+   	} else {
+   		returnFactorization(inputNumber);
+    }
+   	printf("\n");
 	return 0;
 }
 
+//Actually calculate prime factors
 void returnFactorization(int inputNumber){
-	printf("Factors of %d: \n", inputNumber);
+	for(int i = 2; i <= inputNumber; i++){
+		//printf("%d / %d = %d\n", inputNumber, i, (inputNumber % i));
+		if(inputNumber % i == 0){
+			printf("%d ", i);
+			returnFactorization(inputNumber / i);
+			break;
+		}
+	}
 }
